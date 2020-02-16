11ff738eda63a36bcb7d45d70758cfe7c23685c8
Singedpls <59855822+Singedpls@users.noreply.github.com>
2020-02-16 14:47:46 -0600

    Create IF_ELIF_ELSE.py

---
+++ IF_ELIF_ELSE.py
@@ -0,0 +1,12 @@
+# Buscar regex y ocultar contraseña en mi caso
+
+contrasena = str(input("Ingresa la contraseña: "))
+
+if contrasena =="Singedpls":
+    print("Contraseña Ingresada Correctamente")
+elif contrasena =="singedpls":
+    print("Recuerda ocupar mayúsculas y minúsculas")
+elif contrasena == "SINGEDPLS":
+    print("Recuerda ocupar mayúsculas y minúsculas")
+else:
+    print("Contraseña Incorrecta!")
